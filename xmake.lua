b2dec338aaaadf18ae11413f8571a31e25f0dee2
engsr6982 <pwxg286704@163.com>
2025-08-05 15:32:37 +0800

    build: update format_version, version, and dependencies

--- xmake.lua
+++ xmake.lua
@@ -7,11 +7,11 @@ add_repositories("engsr6982-repo https://github.com/engsr6982/xmake-repo.git")
 -- add_requires("levilamina x.x.x") for a specific version
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
-add_requires("levilamina 1.4.1", {configs = {target_type = "server"}})
+add_requires("levilamina 1.3.1", {configs = {target_type = "server"}})
 add_requires("levibuildscript")
 add_requires("legacyremotecall 0.9.0-rc.1")
 
-local PLandVersion = "0.12.0"
+local PLandVersion = "0.11.0"
 add_requires("pland "..PLandVersion)
 
 if not has_config("vs_runtime") then
